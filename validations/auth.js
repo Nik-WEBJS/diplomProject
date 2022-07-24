6b5b72c033b504631fa2c2d87fa4fd95ed987551
Nik <nikita.fokin0123@gmail.com>
2022-07-25 00:42:52 +0300

    posts functionality added

--- validations/auth.js
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator"
-
-export const registerValidator = [
-    body('email', 'неверный формат почты').isEmail(),
-    body('password','минимум 5 символов').isLength({min:5}),
-    body('fullName','минимум 3 символа').isLength({min:3}),
-    body('avatarUrl','Неверная ссылка на аватарку').optional().isURL(),
-];
